417e457d378c9f697290ab0efb9c1321ff0f8f5e
Saad-al-abeed <saadalabeed1737@gmail.com>
2026-05-08 20:14:36 +0600

    removing unnecessary imports

--- src/main/java/org/custombrowser/Main.java
+++ src/main/java/org/custombrowser/Main.java
@@ -1,41 +1,16 @@
 package org.custombrowser;
 
-import java.util.Scanner;
-
-import org.custombrowser.cssomparser.CssomParser;
-import org.custombrowser.domparser.DomParser;
-import org.custombrowser.network.NetworkManager;
-import org.jsoup.nodes.Document;
-import javafx.application.Application;
 import org.custombrowser.ui.BrowserWindow;
+
+import javafx.application.Application;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        // if (args.length > 0 && args[0].equals("--console")) {
-        //     Scanner scanner = new Scanner(System.in);
-        //     NetworkManager networkManager = new NetworkManager();
-        //     DomParser domParser = new DomParser();
-        //     CssomParser cssomParser = new CssomParser();
+        
+        Application.launch(BrowserWindow.class, args);
 
-        //     System.out.println("Enter a url:");
-        //     String url = scanner.nextLine();
-        //     System.out.println("Fetching: " + url);
-            
-        //     networkManager.fetchPage(url)
-        //             .thenAccept(html -> {
-        //                 if (html != null) {
-        //                     Document doc = domParser.parse(html);
-        //                     cssomParser.parse(doc);
-        //                 }
-        //             }).join();
-
-        //     scanner.close();
-        // } else {
-            // Launch the JavaFX application
-            Application.launch(BrowserWindow.class, args);
-        // }
     }
 
 }
--- src/main/java/org/custombrowser/cssomparser/CssomParser.java
+++ src/main/java/org/custombrowser/cssomparser/CssomParser.java
@@ -1,16 +1,15 @@
 package org.custombrowser.cssomparser;
 
-import com.helger.css.ECSSVersion;
-import com.helger.css.decl.CascadingStyleSheet;
-import com.helger.css.decl.CSSStyleRule;
-import com.helger.css.reader.CSSReader;
-import com.helger.css.writer.CSSWriterSettings;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.helger.css.ECSSVersion;
+import com.helger.css.decl.CascadingStyleSheet;
+import com.helger.css.reader.CSSReader;
 
 public class CssomParser {
 
@@ -27,7 +26,7 @@ public class CssomParser {
 
         System.out.println("Found " + styleElements.size() + " style blocks in this page");
 
-        CSSWriterSettings writerSettings = new CSSWriterSettings(ECSSVersion.CSS30);
+        //CSSWriterSettings writerSettings = new CSSWriterSettings(ECSSVersion.CSS30);
 
         for (Element styE : styleElements) {
             String cssContent = styE.html();
@@ -35,14 +34,6 @@ public class CssomParser {
             
             if (css != null) {
                 cssom.add(css);
-                
-                // optional code for testing (prints all CSS rule to terminal)
-                // System.out.println("Found " + css.getStyleRuleCount() + " number of CSS rules in this block");
-                // System.out.println("Listing them below...");
-                
-                // for (CSSStyleRule rule : css.getAllStyleRules()) { // iterates over every CSS rule inside the block
-                //     System.out.println(rule.getAsCSSString(writerSettings, 0));
-                // }
             }
         }
         
